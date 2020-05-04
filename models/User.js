7dcd9e6ba9e07a0f7c18b441f0bfbb26ee0f5c6f
Emma Price <emma.price@theappbusiness.com>
2020-05-04 17:24:11 +0100

    added billing and client side app

--- models/User.js
+++ models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-
 const { Schema } = mongoose
 
 const userScema = new Schema ({
-    googleId: String
+    googleId: String,
+    credits: { type: Number, default: 0 }
 })
 
 mongoose.model('users', userScema)
