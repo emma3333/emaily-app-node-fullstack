0c2124c45e7909fcf2deea64c03ce5cc3e41198f
Emma Price <emma.price@theappbusiness.com>
2020-05-11 14:16:05 +0100

    Users can see survey results

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', noValueErrorMsg: 'You must provide a title' },
+    { label: 'Subject Line', name: 'subject', noValueErrorMsg: 'You must provide a subject' },
+    { label: 'Email Body', name: 'body', noValueErrorMsg: 'You must provide a body' },
+    { label: 'Recipient List', name: 'recipients', noValueErrorMsg: 'You must provide an email address' }
+]
